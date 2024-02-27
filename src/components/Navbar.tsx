488435eb270afd458c93a4dfa21a7ce15a0f6004
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-02-28 00:26:40 +0530

    final fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,6 @@ import {
 } from '@kinde-oss/kinde-auth-nextjs/server'
 import {ArrowRight } from "lucide-react"
 import UserAccountNav from './UserAccountNav'
-import MobileNav from './MobileNav'
 
 const Navbar = () => {
   const { getUser } = getKindeServerSession()
