86e9137ea05aa3c767f46aefb9a737a3e8dcf721
dhruvsanan <21f1004102@student.onlinedegree.iitm.ac.in>
2024-02-28 00:17:56 +0530

    final fixes

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/sign-in',
-        destination: '/api/auth/login',
-        permanent: true,
-      },
-      {
-        source: '/sign-up',
-        destination: '/api/auth/register',
-        permanent: true,
-      },
-    ]
-  },
-
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, webpack }
@@ -23,6 +8,9 @@ const nextConfig = {
     config.resolve.alias.encoding = false
     return config
   },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  }
 }
 
 module.exports = nextConfig
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,46 @@ export function absoluteUrl(path: string) {
     process.env.PORT ?? 3000
   }${path}`
 }
+export function constructMetadata({
+  title = "ChatPdf - the SaaS for students",
+  description = "ChatPdf is an open-source software to make chatting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@dhruvsanan"
+    },
+    icons,
+    metadataBase: new URL('https://chatpdf-dhruv.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
+}
